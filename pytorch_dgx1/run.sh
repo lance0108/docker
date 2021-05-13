0bd71f365dce1f75a82530c84aabb248b5992146
lance0108 <ldeng@wish.com>
2021-05-12 17:46:47 -0700

    jupyter use jedi = false

--- pytorch_dgx1/run.sh
+++ pytorch_dgx1/run.sh
@@ -1,12 +1,13 @@
 docker build -t ldeng_pytorch .
 docker run \
-  --gpus '"device=5,6"' \
+  --gpus '"device=0,1,6,7"' \
   -d \
   --rm \
   --shm-size=1g \
   --ulimit memlock=-1 \
   --ulimit stack=67108864 \
   --net=host \
+  -e PYTHONPATH=$PYTHONPATH:/workspace/wip \
   -v /data/ldeng:/workspace \
   --name ldeng_pytorch \
   ldeng_pytorch
